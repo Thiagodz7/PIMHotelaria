3da0743c76380b2ac1037a5849ab08b02676dce0
vgcazevedo <vitoriagcazevedo@gmail.com>
2021-10-29 01:32:21 -0300

    0.0.3 29.10.21

--- Data/PIMHotelariaContext.cs
+++ Data/PIMHotelariaContext.cs
@@ -16,11 +16,6 @@ namespace PIMHotelaria.Data
         {
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-EFOV76KH;
-             Initial Catalog=BDPimHotelariat;Integrated Security=true");
-        }
 
         public DbSet<PIMHotelaria.Pages.Clientes.Clientes> Clientes { get; set; }
 
